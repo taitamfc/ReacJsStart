59dd1782ec147e611b66ed19e135339d5792ecf4
taitamfc <taitam8590@gmail.com>
2022-10-09 19:53:13 +0700

    CT01Variables

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
+
 import './App.css';
+import ColorBox from "./components/ColorBox";
+import CT01Variables from "./components/CT01Variables";
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Taitam
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CT01Variables></CT01Variables>
+      <ColorBox color="red"></ColorBox>
+      <ColorBox color="yellow" rounded={false}></ColorBox>
     </div>
   );
 }
